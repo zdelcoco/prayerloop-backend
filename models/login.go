e7fb7b919cb3610bd0682d133863b0e2cf24b8c8
Zachary Delcoco <zdelcoco@dmsi.com>
2025-11-26 13:58:44 -0600

    Use email instead of username for login/signup

--- models/login.go
+++ models/login.go
@@ -1,6 +1,7 @@
 package models
 
 type Login struct {
-	Username string `json:"username" binding:"required"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
 	Password string `json:"password" binding:"required"`
 }
