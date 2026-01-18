708f7a2f4d040a226e37858d6e3d9b28ff6fed32
Zachary Delcoco <zdelcoco@dmsi.com>
2026-01-18 10:33:46 -0600

    fix(02-03): add db: struct tags to Prayer, UserPrayer, PrayerAccess
    - Add db: tags to Prayer struct for goqu ScanStruct mapping
    - Add db: tags to UserPrayer struct for joined query results
    - Add db: tags to PrayerAccess struct for consistency
    - Fixes Created_By field not being populated on prayer fetch

--- models/prayer.go
+++ models/prayer.go
@@ -3,48 +3,48 @@ package models
 import "time"
 
 type Prayer struct {
-	Prayer_ID                 int        `json:"prayerId" goqu:"skipinsert"`
-	Prayer_Type               string     `json:"prayerType"`
-	Is_Private                *bool      `json:"isPrivate"`
-	Title                     string     `json:"title"`
-	Prayer_Description        string     `json:"prayerDescription"`
-	Is_Answered               *bool      `json:"isAnswered"`
-	Prayer_Priority           *int       `json:"prayerPriority"`
-	Prayer_Subject_ID         *int       `json:"prayerSubjectId"`
-	Subject_Display_Sequence  int        `json:"subjectDisplaySequence"`
-	Datetime_Answered         *time.Time `json:"datetimeAnswered"`
-	Created_By                int        `json:"createdBy"`
-	Datetime_Create           time.Time  `json:"datetimeCreate" goqu:"skipinsert"`
-	Updated_By                int        `json:"updatedBy"`
-	Datetime_Update           time.Time  `json:"datetimeUpdate" goqu:"skipinsert"`
-	Deleted                   bool       `json:"deleted" goqu:"skipinsert"`
+	Prayer_ID                int        `json:"prayerId" db:"prayer_id" goqu:"skipinsert"`
+	Prayer_Type              string     `json:"prayerType" db:"prayer_type"`
+	Is_Private               *bool      `json:"isPrivate" db:"is_private"`
+	Title                    string     `json:"title" db:"title"`
+	Prayer_Description       string     `json:"prayerDescription" db:"prayer_description"`
+	Is_Answered              *bool      `json:"isAnswered" db:"is_answered"`
+	Prayer_Priority          *int       `json:"prayerPriority" db:"prayer_priority"`
+	Prayer_Subject_ID        *int       `json:"prayerSubjectId" db:"prayer_subject_id"`
+	Subject_Display_Sequence int        `json:"subjectDisplaySequence" db:"subject_display_sequence"`
+	Datetime_Answered        *time.Time `json:"datetimeAnswered" db:"datetime_answered"`
+	Created_By               int        `json:"createdBy" db:"created_by"`
+	Datetime_Create          time.Time  `json:"datetimeCreate" db:"datetime_create" goqu:"skipinsert"`
+	Updated_By               int        `json:"updatedBy" db:"updated_by"`
+	Datetime_Update          time.Time  `json:"datetimeUpdate" db:"datetime_update" goqu:"skipinsert"`
+	Deleted                  bool       `json:"deleted" db:"deleted" goqu:"skipinsert"`
 }
 
 type UserPrayer struct {
-	User_Profile_ID          int        `json:"userProfileId" goqu:"skipinsert"`
-	Prayer_ID                int        `json:"prayerId" goqu:"skipinsert"`
-	Prayer_Access_ID         int        `json:"prayerAccessId" goqu:"skipinsert"`
-	Display_Sequence         int        `json:"displaySequence" goqu:"skipinsert"`
-	Subject_Display_Sequence int        `json:"subjectDisplaySequence" goqu:"skipinsert"`
-	Prayer_Type              string     `json:"prayerType"`
-	Is_Private               *bool      `json:"isPrivate"`
-	Title                    string     `json:"title"`
-	Prayer_Description       string     `json:"prayerDescription"`
-	Is_Answered              *bool      `json:"isAnswered"`
-	Prayer_Priority          *int       `json:"prayerPriority"`
-	Prayer_Subject_ID              *int       `json:"prayerSubjectId" goqu:"skipinsert"`
-	Prayer_Subject_Display_Name    *string    `json:"prayerSubjectDisplayName,omitempty" goqu:"skipinsert"`
+	User_Profile_ID                int        `json:"userProfileId" db:"user_profile_id" goqu:"skipinsert"`
+	Prayer_ID                      int        `json:"prayerId" db:"prayer_id" goqu:"skipinsert"`
+	Prayer_Access_ID               int        `json:"prayerAccessId" db:"prayer_access_id" goqu:"skipinsert"`
+	Display_Sequence               int        `json:"displaySequence" db:"display_sequence" goqu:"skipinsert"`
+	Subject_Display_Sequence       int        `json:"subjectDisplaySequence" db:"subject_display_sequence" goqu:"skipinsert"`
+	Prayer_Type                    string     `json:"prayerType" db:"prayer_type"`
+	Is_Private                     *bool      `json:"isPrivate" db:"is_private"`
+	Title                          string     `json:"title" db:"title"`
+	Prayer_Description             string     `json:"prayerDescription" db:"prayer_description"`
+	Is_Answered                    *bool      `json:"isAnswered" db:"is_answered"`
+	Prayer_Priority                *int       `json:"prayerPriority" db:"prayer_priority"`
+	Prayer_Subject_ID              *int       `json:"prayerSubjectId" db:"prayer_subject_id" goqu:"skipinsert"`
+	Prayer_Subject_Display_Name    *string    `json:"prayerSubjectDisplayName,omitempty" db:"prayer_subject_display_name" goqu:"skipinsert"`
 	Prayer_Subject_User_Profile_ID *int       `json:"prayerSubjectUserProfileId,omitempty" db:"prayer_subject_user_profile_id" goqu:"skipinsert"`
-	Datetime_Answered           *time.Time `json:"datetimeAnswered"`
-	Created_By               int        `json:"createdBy"`
-	Datetime_Create          time.Time  `json:"datetimeCreate" goqu:"skipinsert"`
-	Updated_By               int        `json:"updatedBy"`
-	Datetime_Update          time.Time  `json:"datetimeUpdate" goqu:"skipinsert"`
-	Deleted                  bool       `json:"deleted" goqu:"skipinsert"`
-	Prayer_Category_ID       *int       `json:"prayerCategoryId,omitempty" goqu:"skipinsert"`
-	Category_Name            *string    `json:"categoryName,omitempty" goqu:"skipinsert"`
-	Category_Color           *string    `json:"categoryColor,omitempty" goqu:"skipinsert"`
-	Category_Display_Seq     *int       `json:"categoryDisplaySequence,omitempty" goqu:"skipinsert"`
+	Datetime_Answered              *time.Time `json:"datetimeAnswered" db:"datetime_answered"`
+	Created_By                     int        `json:"createdBy" db:"created_by"`
+	Datetime_Create                time.Time  `json:"datetimeCreate" db:"datetime_create" goqu:"skipinsert"`
+	Updated_By                     int        `json:"updatedBy" db:"updated_by"`
+	Datetime_Update                time.Time  `json:"datetimeUpdate" db:"datetime_update" goqu:"skipinsert"`
+	Deleted                        bool       `json:"deleted" db:"deleted" goqu:"skipinsert"`
+	Prayer_Category_ID             *int       `json:"prayerCategoryId,omitempty" db:"prayer_category_id" goqu:"skipinsert"`
+	Category_Name                  *string    `json:"categoryName,omitempty" db:"category_name" goqu:"skipinsert"`
+	Category_Color                 *string    `json:"categoryColor,omitempty" db:"category_color" goqu:"skipinsert"`
+	Category_Display_Seq           *int       `json:"categoryDisplaySequence,omitempty" db:"category_display_sequence" goqu:"skipinsert"`
 }
 
 type PrayerCreate struct {
@@ -59,15 +59,15 @@ type PrayerCreate struct {
 }
 
 type PrayerAccess struct {
-	Prayer_Access_ID  int       `json:"prayerAccessId" goqu:"skipinsert"`
-	Prayer_ID         int       `json:"prayerId"`
-	Access_Type       string    `json:"accessType"`
-	Access_Type_ID    int       `json:"accessTypeId"`
-	Display_Sequence  int       `json:"displaySequence"`
-	Datetime_Create   time.Time `json:"datetimeCreate" goqu:"skipinsert"`
-	Datetime_Update   time.Time `json:"datetimeUpdate" goqu:"skipinsert"`
-	Created_By        int       `json:"createdBy"`
-	Updated_By        int       `json:"updatedBy"`
+	Prayer_Access_ID int       `json:"prayerAccessId" db:"prayer_access_id" goqu:"skipinsert"`
+	Prayer_ID        int       `json:"prayerId" db:"prayer_id"`
+	Access_Type      string    `json:"accessType" db:"access_type"`
+	Access_Type_ID   int       `json:"accessTypeId" db:"access_type_id"`
+	Display_Sequence int       `json:"displaySequence" db:"display_sequence"`
+	Datetime_Create  time.Time `json:"datetimeCreate" db:"datetime_create" goqu:"skipinsert"`
+	Datetime_Update  time.Time `json:"datetimeUpdate" db:"datetime_update" goqu:"skipinsert"`
+	Created_By       int       `json:"createdBy" db:"created_by"`
+	Updated_By       int       `json:"updatedBy" db:"updated_by"`
 }
 
 type PrayerAccessCreate struct {
